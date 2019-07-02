caba84b569f9d83240f5a55f77e1ff337fb0d4b8
Martin Kromm <martin.kromm@stud.tu-darmstadt.de>
2019-07-02 08:44:03 +0200

    Changed the license of the repository from GNU Affero General public License to GNU Lesser General Public License

--- ot/alsz-ot-ext-snd.h
+++ ot/alsz-ot-ext-snd.h
@@ -1,10 +1,19 @@
-/*
- * alsz-ot-ext-snd.h
- *
- *  Created on: Mar 23, 2015
- *      Author: mzohner
- *
- * Malicious OT extension routine from ALSZ15
+/**
+ \file 		alsz-ot-ext-snd.cpp
+ \author	michael.zohner@ec-spride.de
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            ABY is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		Malicious OT extension routine from ALSZ15
  */
 
 #ifndef ALSZ_OT_EXT_SND_H_
